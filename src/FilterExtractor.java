1a682ac9996b893e3538c9edbcdb843c63837bc9
EvilAv <andrey.vookolov85@gmail.com>
2022-05-17 01:15:50 +0300

    try smrh another

--- src/FilterExtractor.java
+++ src/FilterExtractor.java
@@ -3,10 +3,11 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FilterExtractor {
-    private static String coefficientsFilename = "C:\\Users\\andre\\MATLAB\\Projects\\WindInTheElevator\\8.fcf";
+    private static String coefficientsFilename = "C:\\Users\\andre\\MATLAB\\Projects\\WindInTheElevator\\1.fcf";
     private static double[] coefficients;
+    private static final int size = 120;
     public static void main(String[] args) {
-        coefficients = new double[77];
+        coefficients = new double[size+1];
         try{
         File coefficientsFile = new File(coefficientsFilename);
         Scanner scanner = new Scanner(coefficientsFile);
@@ -22,10 +23,10 @@ public class FilterExtractor {
         System.out.println("File not found");
         e.printStackTrace();
     }
-        for (int i = 0; i < 19; i++) {
+        for (int i = 0; i < (size/4); i++) {
             System.out.println(coefficients[4*i] + ", " + coefficients[4*i+1] + ", "+ coefficients[4*i+2] + ", "+ coefficients[4*i+3] + ",");
         }
-
+        System.out.println(coefficients[size]);
     }
 
 }
