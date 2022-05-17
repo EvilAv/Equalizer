f10a333da3e3039a60bd2d05b51340aa767d7acc
EvilAv <andrey.vookolov85@gmail.com>
2022-05-17 23:01:03 +0300

    initial

--- src/effects/Effect.java
+++
@@ -1,7 +0,0 @@
-package effects;
-public abstract class Effect {
-    // абстрактный клаас родитель для эффектов
-    protected short[] inputAudioStream;
-    public abstract short[] createEffect();
-    public abstract short[] getOutputAudioStream();
-}
